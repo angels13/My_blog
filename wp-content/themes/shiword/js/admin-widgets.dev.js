fdb49b5ee8df17790163d4aca3be1e737339b72c
zhaoyu187 <zhaoyu7@jd.com>
2018-08-01 13:12:44 +0800

    Revert "master"
    This reverts commit ee09833601da2c7349470c1b35271ffdd707972e.

--- wp-content/themes/shiword/js/admin-widgets.dev.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($){
-    $('#widget-list').append('<p class="clear description" style="border-bottom: 1px solid #ccc;">Shiword widgets</p>');
-    bz_widgets = $('#widget-list .widget[id*=_shi-]');
-    $('#widget-list').append(bz_widgets);
-});
